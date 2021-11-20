227307ef381f560e4c71d785450ec2bacd2941bb
rguezgiova <grodriguez.inf@gmail.com>
2021-11-20 17:20:23 +0000

    Subida de ejercicios

--- AUT2_14_Giovanni_Rodriguez_Cookies/storage.js
+++ AUT2_14_Giovanni_Rodriguez_Cookies/storage.js
@@ -1,34 +1,56 @@
+let contador = 0;
+let storage = window.localStorage;
+
 onload = () => {
-    if (sessionStorage.getItem('usuario') == null) {
-        usuario = prompt("Introduzca su nombre:");
-        nombre = new sessionStorage.setItem('usuario', usuario);
+    login();
+    contadorNuevo();
+}
+
+/**
+ * Funcion que pide un nombre y te logea
+ */
+function login() {
+    if (storage.length === 0) {
+        let nombre = prompt("Introduzca su nombre");
+        if (nombre !== null) {
+            storage.setItem('nombre', nombre);
+            document.getElementById("saludo").innerText = "Bienvenido " + storage.getItem("nombre") + "!";
+        }
     } else {
-        document.getElementById("saludo").innerText = "Otra vez por aquí " + nombre + ", ¿cómo le va?";
+        if (storage.getItem('nombre') !== null && storage.getItem('nombre') !== "" && document.getElementById("saludo").value === undefined) {
+            document.getElementById("saludo").innerText = "Otra vez por aquí " + storage.getItem('nombre') + ", ¿Cómo le va?";
+        }
     }
-    contador = 0;
+}
+
+function contadorNuevo(){
+    if(contador === 0 && storage.getItem("nombre") !== null) {
+        document.getElementById("contador").innerText = "Contador a 0";
+    }
 }
 
 /**
  * Funcion que incrementa en 1 el contador
- * @param valor a incrementar
  */
-function incrementar(valor) {
-    sessionStorage.setItem(valor, contador + 1);
+function incrementarContador() {
+    contador++;
+    document.getElementById("contador").innerText = "Contador a " + contador;
 }
 
 /**
- * Funcion qye decrementar en 1 el contador
- * @param valor a decrementar
+ * Funcion qye decrementar en 1 el contador]]
  */
-function decrementar(valor) {
-    sessionStorage.setItem(valor, contador - 1);
+function decrementarContador() {
+    if(contador > 0) {
+        contador--;
+        document.getElementById("contador").innerText = "Contador a " + contador;
+    }
 }
 
 /**
  * Funcion que vacia las claves
  */
 function logout() {
-    sessionStorage.clear();
+    alert("Hasta la próxima " + storage.getItem('nombre'));
+    storage.clear();
 }
-
-document.getElementById("contador").innerText = "Contador a " + contador;
