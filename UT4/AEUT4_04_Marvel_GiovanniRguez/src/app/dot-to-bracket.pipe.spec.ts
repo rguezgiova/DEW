61cea0aa06f3dae95a6e315ef6baa2e363c9944b
rguezgiova <grodriguez.inf@gmail.com>
2022-01-28 18:29:58 +0000

    Añadido cambios

--- UT4/AEUT4_04_Marvel_GiovanniRguez/src/app/dot-to-bracket.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DotToBracketPipe } from './dot-to-bracket.pipe';
-
-describe('DotToBracketPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DotToBracketPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
