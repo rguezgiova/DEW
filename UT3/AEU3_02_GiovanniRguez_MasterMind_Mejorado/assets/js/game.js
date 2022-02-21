573bc99ec6f09a58974b47bb58881a0653a50499
rguezgiova <grodriguez.inf@gmail.com>
2022-02-21 18:57:01 +0000

    Añadido cambios

--- UT3/AEU3_02_GiovanniRguez_MasterMind_Mejorado/assets/js/game.js
+++ UT3/AEU3_02_GiovanniRguez_MasterMind_Mejorado/assets/js/game.js
@@ -1,34 +1,51 @@
 let game = (function () {
-    let colores = ["rojo", "blanco", "negro", "amarillo", "naranja", "marron", "azul", "verde"];
-    let resultado = [];
+    let colores = ['rojo', 'blanco', 'negro', 'amarillo', 'naranja', 'marron', 'azul', 'verde'];
+    let combinacion = [];
+    let intentos = 0;
 
+    /**
+     * Función que se encarga de generar una combinación aleatoria entre los colores
+     */
     let init = function () {
-        while (resultado.length < 4) {
-            resultado.push(colores[Math.floor(Math.random() * colores.length)]);
+        while (combinacion.length < 4) {
+            combinacion.push(colores[Math.floor(Math.random() * colores.length)]);
         }
-        console.log(resultado);
+        console.log(combinacion);
     }
 
+    /**
+     * Función que se encarga de comprobar el intento con la combinación
+     * @param intento a comprobar
+     * @returns {*[]}
+     */
     let comprobar = function (intento) {
         let resultado = [];
         let tmpIntento = [];
-        let tmpResultado = [];
+        let tmpCombinacion = [];
 
-        intento.forEach((color, posicion) => {
-            if (color === resultado[posicion]) {
-                resultado.push('negro');
-            } else {
-                tmpIntento.push(color);
-                tmpResultado.push(resultado[posicion]);
-            }
-        });
+        if (intentos <= 10) {
+            intento.forEach((color, posicion) => {
+                if (color === combinacion[posicion]) {
+                    resultado.push('negro');
+                } else {
+                    tmpIntento.push(color);
+                    tmpCombinacion.push(combinacion[posicion]);
+                }
+            });
 
-        tmpIntento.forEach((bola) => {
-            if (tmpResultado.includes(bola)) {
-                resultado.push('blanco');
-                tmpResultado[tmpResultado.indexOf(bola)] = undefined;
-            }
-        });
+            tmpIntento.forEach((color) => {
+                if (tmpCombinacion.includes(color)) {
+                    resultado.push('blanco');
+                    tmpCombinacion[tmpCombinacion.indexOf(color)] = undefined;
+                } else {
+                    resultado.push('gris');
+                    tmpCombinacion[tmpCombinacion.indexOf(color)] = undefined;
+                }
+            });
+        } else {
+            alert('Has llegado al número máximo de intentos');
+        }
+        intentos++;
         return resultado;
     }
 
--- UT3/AEU3_02_GiovanniRguez_MasterMind_Mejorado/assets/js/main.js
+++ UT3/AEU3_02_GiovanniRguez_MasterMind_Mejorado/assets/js/main.js
@@ -2,37 +2,59 @@ $(function () {
     game.init();
     let intentos = $('#intentos');
 
+    /**
+     * Función que recoge los inputs del intento actual, al hacer clic sobre ellos se eliminan del intento
+     * @type {*|jQuery}
+     */
     let inputs = $('#input_colores div').click(function () {
         if ($(this).attr('class') !== '') $(this).hide().removeClass().fadeIn();
     });
 
-    let colores = $('#colores div').click(function () {
-        let hueco = $("#input_colores div[class=\"\"]");
-        if (hueco.length !== 0) hueco.first().hide().addClass($(this).attr('class')).fadeIn();
+    /**
+     * Función que recoge los colores al hacer clic en ellos en la paleta y los pinta en el intento
+     * @type {*|jQuery}
+     */
+    let colores = $("#colores div").click(function () {
+        let espacio = $("#input_colores div[class=\"\"]");
+        if (espacio.length !== 0) {
+            espacio.first().hide().addClass($(this).attr('class')).fadeIn();
+        }
     });
 
+    /**
+     * Función que crea un array con los colores del intento o el resultado del mismo
+     * @param tipo del array
+     * @param arrayColores con los colores del intento
+     * @returns {*|jQuery}
+     */
     let crearColores = (tipo, arrayColores) => {
-        let divColores = $('<div></div>').addClass(tipo);
+        let divBolas = $('<div></div>').addClass(tipo);
 
         $(arrayColores).each(function(posicion, color) {
-            divColores.append($('<div></div>').addClass(color));
+            divBolas.append($("<div></div>").addClass(color));
         });
 
         if (arrayColores.length < 4) {
             for (let i = 0; i < 4 - arrayColores.length; i++) {
-                divColores.append($('<div></div>'));
+                divBolas.append($('<div></div>'));
             }
         }
-        return divColores;
+        return divBolas;
     }
 
-    let comprobar = $('#input_comprobar').click(function () {
+    /**
+     * Función que comprueba un intento y lo pinta en el div
+     * @type {*|jQuery}
+     */
+    let $comprobar = $("#input_comprobar").click(function () {
+
         if ($("#input_colores div[class='']").length === 0) {
 
             let intento = [];
-            inputs.each(function(posicion, color) {
-                intento.push($(color).attr('class'));
+            inputs.each(function(posicion, bola) {
+                intento.push($(bola).attr("class"));
             });
+
             let resultado = game.comprobar(intento);
 
             intentos.prepend($('<div></div>')
@@ -41,9 +63,10 @@ $(function () {
                 .append(crearColores('resultado', resultado))
                 .hide()
                 .fadeIn());
+
             inputs.removeClass();
 
-            if (resultado.filter(bola => bola === 'negro').length === 4) {
+            if (resultado.filter(color => color === 'negro').length === 4) {
                 let ganar = $('#ganar');
                 let juego = $('main');
 
@@ -52,7 +75,7 @@ $(function () {
                 juego.click(function () {
                     inputs.off();
                     colores.off();
-                    comprobar.off();
+                    $comprobar.off();
                 });
 
                 $('#volver').click(function () {
@@ -66,4 +89,4 @@ $(function () {
             }
         }
     });
-})
+});
